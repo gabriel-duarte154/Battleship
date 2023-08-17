c585f79bee142359eba5d80d64ed10eaebc25784
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-08-17 08:35:45 -0300

    add new variables for player

--- src/game_logic/player/player.mjs
+++ src/game_logic/player/player.mjs
@@ -1,13 +1,34 @@
 import GameBoard from '../gameboard/gameboard.mjs';
 
-function Player() {
-  const attack = (position, gameboard ) => {
+function Player(name) {
+  let lastShot;
+  let missedShots;
+
+  const attack = (position, gameboard) => {
+    setMissedshots(gameboard);
     gameboard.receiveAttack(position);
+    updateLastShoot(gameboard);
+  };
+
+  const setMissedshots = (gameboard) => {
+    missedShots = gameboard.getMissedShots().length;
+  };
+
+  const updateLastShoot = (gameboard) => {
+    gameboard.getMissedShots().length > missedShots
+      ? (lastShot = 'miss')
+      : (lastShot = 'hit');
+  };
+
+  const getLastShot = () => {
+    return lastShot;
   };
 
   return {
+    name,
     gameboard: GameBoard(),
     attack,
+    getLastShot,
   };
 }
 
