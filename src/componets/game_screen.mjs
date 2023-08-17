6c06a1d59f82427c00b196f83abf127d8d532c75
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-08-17 08:25:07 -0300

    Add new screen components

--- src/componets/game_screen.mjs
+++ src/componets/game_screen.mjs
@@ -1,9 +1,9 @@
 const initial_screen = document.createElement('div');
 initial_screen.innerHTML = `
   <div class="initial_wrapper">
-      <h1 class="intro_title big_font">Battleship</h1>
+      <div class="title_wrapper"><h1 class="intro_title intro_title--animation big_font">Battleship</h1></div>
       <div>
-        <button class="button" id="start_button">Start</button>
+        <button class="button button--animation" id="start_button">Start</button>
       </div>
     </div>
 `;
@@ -30,6 +30,25 @@ game_screen.innerHTML = `
   </div> 
 `;
 
-const place_ship_screen = ``;
+const place_ship_screen = document.createElement('div');
+place_ship_screen.innerHTML = `
+    <div class="wrapper">
+      <div class="wrapper_header">
+        <div class="wrapper_text">Set your <span id="ship_name">Battleship</span></div>
+        <button class="button button--small" id="toggle_axis">Toggle axis</button>
+      </div>
+      <div class="gameboard_wrapper gameboard_wrapper--center">
+        <div class="gameboard"></div>
+      </div>
+    </div>`;
 
-export { initial_screen, place_ship_screen, game_screen };
+const gameOverModal = document.createElement('div');
+gameOverModal.innerHTML = `
+    <div class="game_over-screen">
+      <div class="modal">
+        <div class="modal_text"></div>
+        <button class="button" id="reset">Try Again</button>
+      </div>
+    </div>`;
+
+export { initial_screen, place_ship_screen, game_screen, gameOverModal };
